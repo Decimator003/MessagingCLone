56419a442496f96435a132f0847630ce10c01e54
Decimator003 <varad.bane2003@gmail.com>
2024-11-15 00:11:54 +0530

    Work in Progress

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,27 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+
 import authRoutes from './routes/auth.routes.js';
+import messageRoutes from './routes/message.routes.js';
+import userRoutes from './routes/user.routes.js';
+
+import connectToMongoDB from './db/connectToMongoDB.js';
+
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 // Load config
 dotenv.config();
-const PORT = process.env.PORT || 5000;
-
-app.get('/', (req, res) => {
-    res.send("Hello World");
-});
+app.use(express.json());//to parse incoming request bodies from req.body
+app.use(cookieParser());
 
 app.use('/api/auth', authRoutes);
+app.use('/api/messages', messageRoutes);
+app.use('/api/users', userRoutes);
 
 
-app.listen(PORT, () => console.log('Server running on port ${PORT}'));
+app.listen(PORT, () => {
+    connectToMongoDB();
+    console.log(`Server is running on port ${PORT}`);
+});
